9cad524228503bca7dc94e085941ce003e2a8a2d
cessadev <cesarurb.dev@gmail.com>
2025-06-29 11:04:23 -0500

    hello world & variables

---
+++ fundamentals/variables/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Strings (string)
+	var name string = "Kyle" // or name := "Kyle" -> Type inference
+	fmt.Println(name)
+
+	//Integer (int)
+	var age int = 26 // or age := 26 -> Type inference
+	fmt.Println(age)
+
+	//Float (float64)
+	var height float64 = 1.75 // or height := 1.75 -> Type inference
+	fmt.Println(height)
+
+	//Boolean (bool)
+	var isMarried bool = false // or isMarried := false -> Type inference
+	fmt.Println(isMarried)
+}
